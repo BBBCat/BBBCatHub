e7ef09997f9fc8484f590423ed440ee3638a305f
BBBCat <645124539@qq.com>
2016-06-16 10:01:33 +0800

    TryNewGit

---
+++ TryNewGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TryNewGit
+//
+//  Created by cjq on 16/6/16.
+//  Copyright © 2016年 cjq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TryNewGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TryNewGit
+//
+//  Created by cjq on 16/6/16.
+//  Copyright © 2016年 cjq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
